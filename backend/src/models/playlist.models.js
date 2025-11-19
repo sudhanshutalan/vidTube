4c7d071dc9ecc4e0866317b040276aef5e3a6750
Sudhanshu Talan <sudhanshutalan@gmail.com>
2025-11-19 23:07:30 +0530

    created playlist model

---
+++ backend/src/models/playlist.models.js
@@ -0,0 +1,36 @@
+/*
+  id string pk
+  owner ObjectId users
+  videos ObjectId[] videos
+  name string
+  description string
+  createdAt Date
+  updatedAt Date */
+
+import mongoose, { Schema } from "mongoose";
+
+const playlistSchema = new Schema(
+  {
+    owner: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+    },
+    videos: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "Video",
+      },
+    ],
+    playlistName: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+  },
+  { timestamps: true },
+);
+
+export const Playlist = mongoose.model("Playlist", playlistSchema);
