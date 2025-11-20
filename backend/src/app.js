27faea822a3624ca2e94ea36b70bef19e656a3dd
Sudhanshu Talan <sudhanshutalan@gmail.com>
2025-11-20 13:34:53 +0530

    implemented file handling and upload with multer and cloudinary

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,6 +1,7 @@
 import express from "express";
 import cors from "cors";
 import healthcheckRouter from "./routes/healthcheck.routes.js";
+import cookieParser from "cookie-parser";
 
 const app = express();
 
@@ -9,6 +10,7 @@ const app = express();
 app.use(express.json({ limit: "16kb" }));
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
 app.use(express.static("public"));
+app.use(cookieParser);
 
 // cors configuration
 app.use(
